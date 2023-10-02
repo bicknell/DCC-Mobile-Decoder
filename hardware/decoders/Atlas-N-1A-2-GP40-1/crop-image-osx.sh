39b37e2ad3ddbbe6e196b2833fee83ed79f90f32
Leo Bicknell <bicknell@ufp.org>
2023-10-02 12:21:35 -0400

    Initial draft of the decoder.

---
+++ hardware/decoders/Atlas-N-1A-2-GP40-1/crop-image-osx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin
+
+# sips - scriptable image processing system
+# -Z 1600 resizes the entire image to a max of 1600 on a side.
+# --cropOffset is the upper left corner of the crop on the resized image.
+# -c is the height and width of the cropped area on the resized image.
+
+sips -Z 1600 --cropOffset 200 480 -c 910 640 Breadboard-1.jpg
